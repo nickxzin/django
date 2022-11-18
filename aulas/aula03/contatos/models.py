2956d5df1482ba0b382d8d25e14dc9461ffcf569
nicolasp <84407787+nickxzin@users.noreply.github.com>
2022-11-18 17:12:36 -0300

    Fazendo Um bglh foda

--- aulas/aula03/contatos/models.py
+++ aulas/aula03/contatos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+from django.utils import timezone
+
+#from django.utils
+
 # Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    Telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    dataCriacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome+" "+self.sobrenome
